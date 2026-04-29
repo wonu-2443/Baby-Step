3dccc1a04ace48244f405ddf9ec33218bbb63a76
wonu-2443 <moposoccer24@gmail.com>
2026-04-29 21:41:04 +0900

    見た目整えた、削除と細分化タスククリックイベントと子タスクのデータ詳細設定、を追加した。 削除ボタンの関数はまだ書いていない、後ろに書く予定

--- newscript.js
+++ newscript.js
@@ -1,29 +1,11 @@
 /*
 //１から次はデータ型にしよう！！
+
 let tasks = []; //タスクを保存する場所
 
 const taskInput = document.getElementById("task_input");
 const button = document.getElementById("add");
 const taskList = document.getElementById("task_list");
-
-//親タスク追加
-button.addEventListener("click", () => {
-    const text = taskInput.value.trim(); //入力値をとって、前後の空白を消す
-
-    if (text !== "") {
-        const newTask = {
-            id: Date.now(),
-            text: text,
-            children: []
-        };
-
-        tasks.push(newTask); //配列の追加
-
-        taskInput.value = ""; //入力欄をリセット
-        render();　//表示、更新
-        };
-    };
-});
 
 //render表示
 
@@ -34,6 +16,25 @@ function render() {
         taskList.appendChild(createTaskElement(task)); //taskListに新しく作ったtask要素を追加
     });
 };
+
+//親タスク追加
+button.addEventListener("click", () => {
+    const text = taskInput.value.trim(); //入力値をとって、前後の空白を消す
+
+    if (text !== "") {
+        const newTask = {
+            id: Date.now(),
+            text: text,
+            children: []
+        }; //テキストの内容を指定
+
+        tasks.push(newTask); //配列の追加
+
+        taskInput.value = ""; //入力欄をリセット
+        render();　//表示、更新
+        };
+    };
+});
 
 
 //DOM作成関数
@@ -47,11 +48,43 @@ function createTaskElement(task) {
 
     //削除ボタン
     const delbtn = document.createElement("button");
-    delbtn.textCOntent ="削除";
+    delbtn.textContent ="削除";
 
     delbtn.addEventListener("click",() => {
-        delbtn.Task(task.id,tasks);
+        delbtnTask(task.id,tasks); //削除関数を呼び出す　関数は最後にまとめる
         render();
     });
-    
-}
+};
+
+li.appendChild(delbtn);
+
+const addSubtaskbtn = document.createElement("button");
+addSubtaskbtn.textContent = "細分化";
+
+addSubtaskbtn.addEventListener("click", () => {
+    const subInput = document.createElement("input");
+    subInput.placeholder = "細分化タスクを入力";
+
+    li.appendChild(subInput);
+
+    subInput.addEventListener("keydown", (e) => {
+        if (e.key === "Enter") {
+            const text = subInput.value.trim();
+
+            if (text !== "") {
+                const child = {
+                    id: Date.now(),
+                    text: text,
+                    children: []
+                };
+
+                task.children.push(child);
+                render();
+                };
+            };
+        };
+    });
+});
+
+li.removeChild(subInput);
+render();
